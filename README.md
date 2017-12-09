e725ed7e4dce32e5fc8397a5b4a501bd061c4db7
flexion <github.account@flexion.ch>
2017-12-09 20:55:43 +0100

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # volumio-mqtt
 NodeJS MQTT Adapter for volumio.org Player
+
+
+# Configure
+Copy config.js.sample to config.js and adjust your settings. 
+config.js is in the gitignore list and will not be overwritten. 
+
+
+# MQTT Topics:
+
+Setters:
+
+- {devicename}/set/volume/percent       0 - 100
+- {devicename}/set/volume/mute          true | false
+- {devicename}/set/volume/push          + | -
+
+- {devicename}/set/play
+- {devicename}/set/stop
+- {devicename}/set/pause
+
+Getters: 
+
+- {devicename}/get/status
+- {devicename}/get/browsesources
+- {devicename}/get/browselibrary
+- {devicename}/get/multiroomdevices
+
+Status responses:
+
+- {devicename}/status/connected         true | false
+- {devicename}/status/info
+- {devicename}/status/browsesources
+- {devicename}/status/browselibrary
+- {devicename}/status/multiroomdevices
+
+
+Volumio player API reference
+https://volumio.github.io/docs/API/WebSocket_APIs.html
---
+++ server.js
@@ -0,0 +1,60 @@
+var version = "2017-12-09 20:52";
+var config = require('./config');
+var mqtt = require('mqtt');
+var io=require('socket.io-client');
+var socket= io.connect(config.volumio_host);
+
+var mqttClient = mqtt.connect(config.mqttHost);
+
+mqttClient.subscribe(config.mqtt_devicename+"/set/#");
+mqttClient.subscribe(config.mqtt_devicename+"/get/#");
+
+mqttClient.on('message', function (topic, rawMessage) {
+    msg = rawMessage.toString();
+    if (config.debug) console.log("[MQTT] Received topic:" + topic.toString(), msg);
+    var arr = topic.split('/');
+    var action = arr[2];
+    if (arr[1]=="set") {        
+        if (action==="volume") { // numeric value between 0 and 100, mute, umute, +, -
+            if (arr[3]==="percent") {
+                console.log("[MQTT] setVolume", msg);
+                socket.emit('volume', Number(msg) );
+            } else if (arr[3]==="mute") { // true | false
+                socket.emit('volume', msg==="true"?"mute":"umute" );
+            } else if (arr[3]==="push") { // + | -
+                socket.emit('volume', msg );
+            }
+        } else if (action=="play") {
+            socket.emit('play');
+        } else if (action=="pause") {
+            socket.emit('pause');
+        } else if (action=="stop") {
+            socket.emit('stop');
+        }
+    } else if (arr[1]==="get") {
+        if (arr[2]==="status") {
+            socket.emit('getState');      
+        } else if (arr[2]==="multiroomdevices") {
+            socket.emit('getMultiRoomDevices');
+        } else if (arr[2]==="browsesources") {
+            socket.emit('getBrowseSources');
+        }
+    }
+});
+socket.on('connect', function(){
+    console.log("["+config.mqtt_devicename+"] Connected");
+    mqttClient.publish(config.mqtt_devicename+"/status/connected", "true", { retain: false });
+});
+socket.on('disconnect', function(){
+    console.log("["+config.mqtt_devicename+"] Disconnected");
+    mqttClient.publish(config.mqtt_devicename+"/status/connected", "false", { retain: false });
+});
+socket.on('pushState', function(data){
+    mqttClient.publish(config.mqtt_devicename+"/status/info", JSON.stringify(data), { retain: false });
+});
+socket.on('pushMultiRoomDevices', function(data){
+    mqttClient.publish(config.mqtt_devicename+"/status/multiroomdevices", JSON.stringify(data), { retain: false });
+});
+socket.on('pushBrowseSources', function(data){
+    mqttClient.publish(config.mqtt_devicename+"/status/browsesources", JSON.stringify(data), { retain: false });
+});
